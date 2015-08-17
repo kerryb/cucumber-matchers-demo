183c810e2cf70f1d11a8add6a86cff59c50bee32
Kerry Buckley <kerryjbuckley@gmail.com>
2015-08-17 15:49:07 +0100

    Check content of rows (terrible failure message)

--- features/support/csv_step_helper.rb
+++ features/support/csv_step_helper.rb
@@ -1,6 +1,10 @@
 require "rspec/matchers"
 
 module CsvStepHelper
+  def row_for csv, widget
+    csv.find {|row| row["Code"] == widget.code }
+  end
+
   RSpec::Matchers.define :have_row_for do |widget|
     match do |csv|
       csv.any? {|row| row["Code"] == widget.code }
@@ -8,6 +12,12 @@ module CsvStepHelper
 
     failure_message do
       "Expected CSV to have a row for the widget with code #{widget.code.inspect}"
+    end
+  end
+
+  RSpec::Matchers.define :contain_data_for do |widget|
+    match do |row|
+      row["Name"] == widget.name && row["Price"] == widget.price
     end
   end
 end
