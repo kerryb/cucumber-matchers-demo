edeeb9faba9c280bc054dfc1d642ea15da90194a
Kerry Buckley <kerryjbuckley@gmail.com>
2015-08-17 14:58:53 +0100

    Write something to the file so the failure is realistic

--- lib/widget_exporter.rb
+++ lib/widget_exporter.rb
@@ -1,6 +1,7 @@
 class WidgetExporter
   def self.export_to path
-    CSV.open path, "wb" do |csv|
+    CSV.open path, "wb", write_headers: true, headers: %w(Code Name Price)  do |csv|
+      csv << [nil, nil, nil]
     end
   end
 end
