dda061dbba5a018b84f6ccea4385b453171c811b
Kerry Buckley <kerryjbuckley@gmail.com>
2015-08-17 14:14:33 +0100

    Implement final step – fails because there's no CSV file

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -1,5 +1,6 @@
 require "widget"
 require "widget_exporter"
+require "csv"
 
 Given "some widgets" do
   @widget_1 = Widget.new "ABC123", "Left-handed screwdriver", 499
@@ -12,4 +13,6 @@ When "I export them to CSV" do
 end
 
 Then "the CSV file contains information about my widgets" do
+  csv = CSV.read @csv_path
+  expect(csv).to have_row_for(@widget_1).and have_row_for(@widget_2)
 end
