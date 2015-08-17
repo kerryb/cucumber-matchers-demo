4a337f3e0c9c8d6ac741a341c93cfccc5dde0d81
Kerry Buckley <kerryjbuckley@gmail.com>
2015-08-17 15:03:09 +0100

    Improve matcher failure message

--- features/support/csv_step_helper.rb
+++ features/support/csv_step_helper.rb
@@ -4,6 +4,10 @@ module CsvStepHelper
   RSpec::Matchers.define :have_row_for do |widget|
     match do |csv|
     end
+
+    failure_message do
+      "Expected CSV to have a row for the widget with code #{widget.code.inspect}"
+    end
   end
 end
 World CsvStepHelper
